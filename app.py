33a48282955d6eee4da0ae576b735601a3cad262
Architacode48 <parchita48@gmail.com>
2023-06-01 16:58:37 +0530

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-#import flask library
+#import appask library
 from flask import Flask, render_template, request
-#initialize flask
-fl=Flask(__name__)
+#initialize appask
+app=Flask(__name__)
 #route your webpage
-@fl.route("/")
+@app.route("/")
 def visitors():
 
 	# Load current count
@@ -23,7 +23,7 @@ def visitors():
 # Render HTML with count variable
 
 #route your webpage
-@fl.route("/",methods=["POST"])
+@app.route("/",methods=["POST"])
 def covid_stats():
 	# Load current count
 	counter_read_file = open("count.txt", "r")
@@ -33,6 +33,6 @@ def covid_stats():
 	api_link="https://covidstats-sdbd.onrender.com/?country="+user_input
 	print(api_link)
 	return render_template("index.html",count=visitors_count,image=api_link)
-#add code for executing flask
+#add code for executing appask
 if __name__=="__main__":
-	fl.run()
+	app.run()
